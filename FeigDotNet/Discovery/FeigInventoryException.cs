a499026733042d23d446c86771b0440079da8bfc
adoconnection <adoconnection@gmail.com>
2016-10-10 22:02:59 +0300

    exception

---
+++ FeigDotNet/Discovery/FeigInventoryException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+using FeigDotNet.Exceptions;
+
+namespace FeigDotNet.Discovery
+{
+    public class FeigInventoryException : FeigException
+    {
+        private readonly int errorCode;
+        public int ErrorCode { get; set; }
+
+        public FeigInventoryException()
+        {
+        }
+
+        public FeigInventoryException(int errorCode, string message) : base(message)
+        {
+            this.errorCode = errorCode;
+        }
+
+        public FeigInventoryException(string message) : base(message)
+        {
+        }
+
+        public FeigInventoryException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected FeigInventoryException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
